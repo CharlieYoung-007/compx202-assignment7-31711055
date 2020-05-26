5d7b21c66b0d4763dac549e8a9c5f0d7bb4eacd0
“CharlieYoung-007” <1172373115@qq.com>
2020-05-26 22:27:18 +0800

    I have required an accelerometer to drive the ball move and add a listener to o receive accelerometer information as a phone

--- app/src/main/java/com/example/compx202_assignment7_31711055/MainActivity.java
+++ app/src/main/java/com/example/compx202_assignment7_31711055/MainActivity.java
@@ -3,19 +3,53 @@ package com.example.compx202_assignment7_31711055;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    SensorManager sensorMgr;
+    Sensor accelerometer;
+    private int x;
+    private int y;
+    private int radius;
+    private Paint paint;
+    private double xMove;
+    private double yMove;
+    SensorEventListener accelerometerListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            xMove = event.values[0];
+            yMove = event.values[2];
+            //Treating y as z and vice versa
+            Log.i("TAG", "" + event.values[0] +
+                    ", " + event.values[1] +
+                    ", " + event.values[2]);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
+    private int width;
+    private int height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //Get a listener to receive accelerometer information
+        Log.d("TAG", "Obtained accelerometer " + accelerometer);
 
         //Hide the action bar
         ActionBar actionBar = getSupportActionBar();
@@ -38,17 +72,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorMgr.registerListener(accelerometerListener, accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMgr.unregisterListener(accelerometerListener, accelerometer);
+    }
+
     //Create a graphic view class extending view
     public class GraphicsView extends View {
-        private int x;
-        private int y;
-        private int radius;
-        private Paint paint;
-
         public GraphicsView(Context context) {
             super(context);
-            x = 0;
-            y = 0;
+            x = 50;
+            y = 50;
             radius = 30;
             paint = new Paint();
             paint.setColor(getColor(android.R.color.holo_blue_bright));
@@ -58,15 +100,32 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-
-
             canvas.drawCircle(x, y, radius, paint);
-            x += 2;
-            y += 2;
-            // some code
+            x += xMove;
+            y += yMove;
+
+            //remain the ball on the screen
+            if (x < 40) {
+                x = 40;
+            }
+            if (width - x < 40) {
+                x = width - 40;
+            }
+            if (y < 40) {
+                y = 40;
+            }
+            if (height - y < 40) {
+                y = height - 40;
+            }
             invalidate();
         }
-    }
 
+        @Override
+        protected void onSizeChanged(int newWidth, int newHeight, int oldWidth, int oldHeight) {
+            super.onSizeChanged(newWidth, newHeight, oldWidth, oldHeight);
+            width = newWidth;
+            height = newHeight;
+        }
+    }
 
 }
