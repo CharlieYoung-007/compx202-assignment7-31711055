3ab769c24db7c379f10d2d5e9bac725c022f838c
“CharlieYoung-007” <1172373115@qq.com>
2020-05-27 14:47:31 +0800

    I amended the xMove and yMove to make the right direction

--- app/src/main/java/com/example/compx202_assignment7_31711055/MainActivity.java
+++ app/src/main/java/com/example/compx202_assignment7_31711055/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.compx202_assignment7_31711055;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -15,19 +16,20 @@ import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    SensorManager sensorMgr;
-    Sensor accelerometer;
+
     private int x;
     private int y;
     private int radius;
     private Paint paint;
     private double xMove;
     private double yMove;
+    SensorManager sensorMgr;
+    Sensor accelerometer;
     SensorEventListener accelerometerListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            xMove = event.values[0];
-            yMove = event.values[2];
+            xMove = -event.values[0];
+            yMove = event.values[1];
             //Treating y as z and vice versa
             Log.i("TAG", "" + event.values[0] +
                     ", " + event.values[1] +
@@ -40,50 +42,6 @@ public class MainActivity extends AppCompatActivity {
     };
     private int width;
     private int height;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //Get a listener to receive accelerometer information
-        Log.d("TAG", "Obtained accelerometer " + accelerometer);
-
-        //Hide the action bar
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        //Set sticky fullscreen mode
-        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
-
-        //Get the ConstraintLayout
-        ConstraintLayout root = findViewById(R.id.MainView);
-
-        //Create an instance of the custom view
-        GraphicsView myView = new GraphicsView(this);
-
-        //Add the custom view to the ConstraintLayout
-        root.addView(myView);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorMgr.registerListener(accelerometerListener, accelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorMgr.unregisterListener(accelerometerListener, accelerometer);
-    }
 
     //Create a graphic view class extending view
     public class GraphicsView extends View {
@@ -128,4 +86,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //Get a listener to receive accelerometer information
+        Log.d("TAG", "Obtained accelerometer " + accelerometer);
+
+        //Hide the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //Set sticky fullscreen mode
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
+        //Get the ConstraintLayout
+        ConstraintLayout root = findViewById(R.id.MainView);
+
+        //Create an instance of the custom view
+        GraphicsView myView = new GraphicsView(this);
+
+        //Add the custom view to the ConstraintLayout
+        root.addView(myView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorMgr.registerListener(accelerometerListener, accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMgr.unregisterListener(accelerometerListener, accelerometer);
+    }
 }
